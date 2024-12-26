3b6f6886e9b08e577717c683291d1ad516d6cdf8
mckaragulle <mustafacelalettinkaragulle@gmail.com>
2024-12-26 15:08:36 +0300

    revize

---
+++ app/Livewire/DealerOfficer/DealerOfficers.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Livewire\DealerOfficer;
+
+use App\Services\DealerOfficerService;
+use App\Services\DealerService;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Attributes\On;
+use Livewire\Component;
+
+class DealerOfficers extends Component
+{
+    use LivewireAlert;
+
+    public null|int $dealer_id = null;
+    public null|int $data_id;
+    public bool $is_show = false;
+
+    public function mount($id = null, bool $is_show)
+    {
+        $this->dealer_id = $id;
+        $this->is_show = $is_show;
+    }
+
+    public function render()
+    {
+        return view('livewire.dealer-officer.dealer-officers');
+    }
+
+    #[On('delete-dealer-officer')]
+    function delete($id)
+    {
+        $this->data_id = $id;
+        $this->confirm(
+            'Bu işlemi yapmak istediğinize emin misiniz?',
+            [
+                'onConfirmed' => 'handleConfirmed',
+                'position' => 'center',
+                'toast' => false,
+                'confirmButtonText' => 'Evet',
+                'cancelButtonText' => 'Hayır',
+                'theme' => 'dark',
+            ]
+        );
+    }
+
+    #[On('handleConfirmed')]
+    public function handleConfirmed(DealerOfficerService $dealerOfficerService)
+    {
+        DB::beginTransaction();
+        $data = $dealerOfficerService->findById($this->data_id);
+        try {
+            $dealerOfficerService->delete($this->data_id);
+            $msg = 'Bayi yetkilisi silindi.';
+            session()->flash('message', $msg);
+            $this->alert('success', $msg, ['position' => 'center']);
+            DB::commit();
+
+            if(!is_null($data->files) && count($data->files) > 0){
+                foreach($data->files as $file){
+                    if(Storage::exists($file)){
+                        Storage::delete($file);
+                    }
+                }
+            }
+        } catch (\Exception $exception) {
+            $error = "Bayi yetkilisi silinemedi. {$exception->getMessage()}";
+            session()->flash('error', $error);
+            $this->alert('error', $error);
+            Log::error($error);
+            DB::rollBack();
+        } finally {
+            $this->dispatch('pg:eventRefresh-DealerOfficerTable');
+        }
+    }
+}
---
+++ app/Models/DealerOfficer.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class DealerOfficer extends Model
+{
+    use SoftDeletes, HasFactory, Sluggable, LogsActivity;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => ['name', 'surname']
+            ]
+        ];
+    }
+
+    protected $fillable = [
+        "dealer_id", 
+        "number",
+        "name",
+        "slug",
+        "surname",
+        "title",
+        "phone1",
+        "phone2",
+        "email",
+        "detail",
+        "files",
+        "status",
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'files' => AsArrayObject::class,
+        ];
+    }
+
+    /**
+     * Get the user's first name.
+     */
+    protected function files(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => json_decode($value),
+        );
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll();
+    }
+
+    /**
+     * Get the Dealer.
+     */
+    public function dealer(): BelongsTo
+    {
+        return $this->belongsTo(Dealer::class);
+    }
+}
